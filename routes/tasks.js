1d86533e61c59a38a33888a5509ac4dc80860267
Anh Nguyen <anh.nguyenvan@asnet.com.vn>
2019-03-24 19:53:43 +0700

    Update task and user

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,10 +3,6 @@ module.exports = app => {
 
   app
     .route("/tasks")
-    .all((req, res, next) => {
-      delete req.body.id;
-      next();
-    })
     .get((req, res) => {
       Tasks.findAll({})
         .then(result => {
@@ -19,6 +15,36 @@ module.exports = app => {
         .then(result => {
           res.json(result);
         })
+        .catch(error => {
+          res.status(412).json({ msg: error.message });
+        });
+    });
+
+  app
+    .route("/tasks/:id")
+    .get((req, res) => {
+      Tasks.findOne({ where: req.params })
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({ msg: error.message });
+        });
+    })
+    .put((req, res) => {
+      Tasks.update(req.body, { where: req.params })
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({ msg: error.message });
+        });
+    })
+    .delete((req, res) => {
+      Tasks.destroy({ where: req.params })
+        .then(result => res.sendStatus(204))
         .catch(error => {
           res.status(412).json({ msg: error.message });
         });
