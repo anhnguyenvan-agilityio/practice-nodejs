cca71770d60f99f815a8a98df40413d82eeb1fa6
anhnguyenvan <anh.nguyenvan@asnet.com.vn>
2019-03-27 17:20:01 +0700

    add condition

--- index.js
+++ index.js
@@ -36,31 +36,37 @@ app.get("/:id", function(req, res) {
 
 app.post("/addUser", (req, res) => {
   fs.readFile(__dirname + "/" + "mockUsers.json", "utf8", function(err, data) {
-    data = JSON.parse(data);
-    if (req.body.name) {
-      const userAdded = {
-        id: 4,
-        name: req.body.name,
-        password: "password4",
-        profession: "teacher"
-      };
-      data["user4"] = userAdded;
-      fs.writeFile(
-        __dirname + "/" + "mockUsers.json",
-        JSON.stringify(data),
-        "utf8",
-        function(err) {
-          if (err)
-            res.json({
-              msg: err.message
-            });
-          else res.json(userAdded);
-        }
-      );
-    } else {
+    if (err) {
       res.json({
-        msg: "Format invalid"
+        msg: err.message
       });
+    } else {
+      data = JSON.parse(data);
+      if (req.body.name) {
+        const userAdded = {
+          id: 4,
+          name: req.body.name,
+          password: "password4",
+          profession: "teacher"
+        };
+        data["user4"] = userAdded;
+        fs.writeFile(
+          __dirname + "/" + "mockUsers.json",
+          JSON.stringify(data),
+          "utf8",
+          function(err) {
+            if (err)
+              res.json({
+                msg: err.message
+              });
+            else res.json(userAdded);
+          }
+        );
+      } else {
+        res.json({
+          msg: "Format invalid"
+        });
+      }
     }
   });
 });
@@ -68,18 +74,31 @@ app.post("/addUser", (req, res) => {
 app.delete("/deleteUser/:id", function(req, res) {
   // First read existing users.
   fs.readFile(__dirname + "/" + "mockUsers.json", "utf8", function(err, data) {
-    data = JSON.parse(data);
-    delete data["user" + req.params.id];
-    fs.writeFile(
-      __dirname + "/" + "mockUsers.json",
-      JSON.stringify(data),
-      "utf8",
-      function(err) {
-        res.json({
-          id: req.params.id
-        });
-      }
-    );
+    if (err) {
+      res.json({
+        msg: err.message
+      });
+    }
+    {
+      data = JSON.parse(data);
+      delete data["user" + req.params.id];
+      fs.writeFile(
+        __dirname + "/" + "mockUsers.json",
+        JSON.stringify(data),
+        "utf8",
+        function(err) {
+          if (err) {
+            res.json({
+              msg: err.message
+            });
+          } else {
+            res.json({
+              id: req.params.id
+            });
+          }
+        }
+      );
+    }
   });
 });
 
