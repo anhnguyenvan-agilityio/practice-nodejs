7e6ef54b24e21ebe18663ad6f5f6cead9ddb96ba
anhnguyenvan <anh.nguyenvan@asnet.com.vn>
2019-04-01 17:12:25 +0700

    apply jwt

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 import express from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
 
 const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.set("json spaces", 4);
+
+// FORMAT OF TOKEN
+// Bearer <access_token>
+// Verify token
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    // Next
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+app.post("/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "a",
+    email: "a@gmail.com"
+  };
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
 
 app.get("/listUsers", (req, res) => {
   fs.readFile(__dirname + "/" + "mockUsers.json", "utf8", function(err, data) {
@@ -34,39 +64,50 @@ app.get("/:id", function(req, res) {
   });
 });
 
-app.post("/addUser", (req, res) => {
-  fs.readFile(__dirname + "/" + "mockUsers.json", "utf8", function(err, data) {
+app.post("/addUser", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    console.log(req.token);
+    console.log(err);
     if (err) {
-      res.json({
-        msg: err.message
-      });
+      res.sendStatus(403);
     } else {
-      data = JSON.parse(data);
-      if (req.body.name) {
-        const userAdded = {
-          id: 4,
-          name: req.body.name,
-          password: "password4",
-          profession: "teacher"
-        };
-        data["user4"] = userAdded;
-        fs.writeFile(
-          __dirname + "/" + "mockUsers.json",
-          JSON.stringify(data),
-          "utf8",
-          function(err) {
-            if (err)
-              res.json({
-                msg: err.message
-              });
-            else res.json(userAdded);
+      fs.readFile(__dirname + "/" + "mockUsers.json", "utf8", function(
+        err,
+        data
+      ) {
+        if (err) {
+          res.json({
+            msg: err.message
+          });
+        } else {
+          data = JSON.parse(data);
+          if (req.body.name) {
+            const userAdded = {
+              id: 4,
+              name: req.body.name,
+              password: "password4",
+              profession: "teacher"
+            };
+            data["user4"] = userAdded;
+            fs.writeFile(
+              __dirname + "/" + "mockUsers.json",
+              JSON.stringify(data),
+              "utf8",
+              function(err) {
+                if (err)
+                  res.json({
+                    msg: err.message
+                  });
+                else res.json(authData);
+              }
+            );
+          } else {
+            res.json({
+              msg: "Format invalid"
+            });
           }
-        );
-      } else {
-        res.json({
-          msg: "Format invalid"
-        });
-      }
+        }
+      });
     }
   });
 });
